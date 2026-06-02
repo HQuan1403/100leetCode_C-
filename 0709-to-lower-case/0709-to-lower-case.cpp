456c6a390d90fe4db68149b4848d8d22c00810c2
HQuan1403 <hnquan14@gmail.com>
2026-06-03 05:20:27 +0700

    Time: 0 ms (100%), Space: 8.3 MB (53.91%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.cpp
@@ -0,0 +1,15 @@
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string toLowerCase(string s) {
+        for (char& c : s) {
+            if (c >= 'A' && c <= 'Z') {
+                c = c + ('a' - 'A'); 
+            }
+        }
+        return s;
+    }
+};
