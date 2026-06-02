6706722ad81a8df39428a11d0a7648ef68065cd7
HQuan1403 <hnquan14@gmail.com>
2026-06-03 04:52:16 +0700

    Time: 0 ms (100%), Space: 9.3 MB (49.37%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,15 @@
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int sum_s = 0, sum_t = 0;
+        
+        for (char c : s) sum_s += c;
+        for (char c : t) sum_t += c;
+        
+        return (char)(sum_t - sum_s);
+    }
+};
