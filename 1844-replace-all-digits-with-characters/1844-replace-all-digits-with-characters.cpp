0ed35eed5c0ac3dec3142454ac2b2b2cde7d5014
HQuan1403 <hnquan14@gmail.com>
2026-06-04 14:55:42 +0700

    Time: 0 ms (100%), Space: 8.1 MB (79.05%) - LeetHub

---
+++ 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.cpp
@@ -0,0 +1,13 @@
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string replaceDigits(string s) {
+        for (int i = 1; i < s.length(); i += 2) {
+            s[i] = s[i - 1] + (s[i] - '0');
+        }
+        return s;
+    }
+};
