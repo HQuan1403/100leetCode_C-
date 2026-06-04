f8589740768e5ecaab82cbf32f905c5e74751bc1
HQuan1403 <hnquan14@gmail.com>
2026-06-04 14:51:19 +0700

    Time: 0 ms (100%), Space: 10.7 MB (72.64%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int max_altitude = 0;
+        int current_altitude = 0;
+        
+        for (int g : gain) {
+            current_altitude += g;
+            max_altitude = max(max_altitude, current_altitude);
+        }
+        
+        return max_altitude;
+    }
+};
