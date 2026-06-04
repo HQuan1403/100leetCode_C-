879f9095019d35cb675634df0968ff8362c7c981
HQuan1403 <hnquan14@gmail.com>
2026-06-04 14:53:10 +0700

    Time: 0 ms (100%), Space: 9.4 MB (90.08%) - LeetHub

---
+++ 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int secondHighest(string s) {
+        int first = -1;
+        int second = -1;
+
+        for (char c : s) {
+            if (isdigit(c)) {
+                int digit = c - '0';
+                if (digit > first) {
+                    second = first;
+                    first = digit;
+                } else if (digit < first && digit > second) {
+                    second = digit;
+                }
+            }
+        }
+
+        return second;
+    }
+};
